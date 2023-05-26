58db73f44b22feed0f8594d79f2aa84baa49ca22
hassan3802 <95935407+hassan3802@users.noreply.github.com>
2023-05-26 17:13:55 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,130 @@
+package com.example.supermarket2.models;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.ArrayList;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private String address;
+    private int cvv;
+    private long creditCard;
+    public User() {
+        this.role = "User";
+    }
+
+    public int getCvv() {
+        return this.cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public long getCreditCard() {
+        return this.creditCard;
+    }
+
+    public void setCreditCard(long creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public User(Long id, String username, String email, String password, String address, int cvv, long creditCard) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = "User";
+        this.address = address;
+        this.cvv = cvv;
+        this.creditCard = creditCard;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setRole(String role){
+        this.role = role;
+    }
+    public String getRole(){
+        return role;
+    }
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>(Arrays.asList(new SimpleGrantedAuthority(this.role)));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.supermarket2.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.example.supermarket2.services.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private UserService userservice;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+   
+
+
+public SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
+    http.userDetailsService(userservice)
+    .authorizeRequests()
+    .antMatchers("/supermarket", "/supermarket/saveUser", "/supermarket/reset-pass").permitAll()
+            .antMatchers("/supermarket/adminHomePage")
+            .hasAuthority("Admin")
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/supermarket/login")
+            .usernameParameter("email")
+            .loginProcessingUrl("/supermarket/check-user")
+            .defaultSuccessUrl("/supermarket/homepage")
+            .successHandler(new MyAuthenticationSuccessHandler())
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/supermarket/logout"))
+            .logoutSuccessUrl("/supermarket/login");
+    return http.build();
+}
+}
