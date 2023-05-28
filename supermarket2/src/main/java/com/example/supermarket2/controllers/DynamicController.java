ce67bc2aba7a499414a724f5d0cc14694849348d
YoussefAmrousy <youssef.amrousy@gmail.com>
2023-05-28 04:17:32 +0300

    Add files via upload

--- supermarket2/src/main/java/com/example/supermarket2/controllers/DynamicController.java
+++ supermarket2/src/main/java/com/example/supermarket2/controllers/DynamicController.java
@@ -1,11 +1,13 @@
 package com.example.supermarket2.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.supermarket2.models.User;
 import com.example.supermarket2.repositories.ProductRepo;
 
 @Controller
@@ -21,8 +23,9 @@ public class DynamicController {
         return mav;
     }
     @GetMapping("/adminHomepage")
-    public ModelAndView getAdminPage() {
+    public ModelAndView getAdminPage(@AuthenticationPrincipal User user) {
         ModelAndView mav = new ModelAndView("admin-home-page.html");
+        mav.addObject("user", user);
         return mav;
     }
 }
